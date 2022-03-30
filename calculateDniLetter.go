3c26ffb33e72a3e006d6c3bca8adf7a67fa5f399
Oscar del Amo <oscar.delamo@diagroup.com>
2022-03-30 13:51:47 +0200

    add calculateDniLetter.go

---
+++ calculateDniLetter.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+var dni = 73434769
+
+func main() {
+	letter := calculateDniLetter()
+	fmt.Printf("The letter of dni %d is %s\n", dni, letter)
+}
+
+//calculateDniLetter returns the letter of the dni
+func calculateDniLetter() string {
+	var letters = []string{"T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"}
+	numberOfLetter := dni % 23
+	return letters[numberOfLetter]
+}
